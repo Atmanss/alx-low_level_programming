b702caa9d78be9c3ed47aefc0361025ee3e55b9e
Atmanss <otmanssbariia@gmail.com>
2023-09-08 18:42:12 +0100

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - entry point
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int c;
+
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c + '0');
+	}
+
+
+	putchar('\n');
+	return (0);
+}
