6f5316f8ac451126c3efa77811db33b5bf1cf04f
Atmanss <otmanssbariia@gmail.com>
2023-09-08 19:01:10 +0100

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 (success)
+ *
+ */
+int main(void)
+{
+	char c, b;
+
+	for (c = 0; c <= 9; c++)
+	{
+		putchar(c);
+	}
+
+	for (b = 'a'; b <= 'f' b++)
+	{
+		putchar(b);
+	}
+
+
+
+	putchar('/n');
+	return (0);
+
+}
