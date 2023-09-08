8a61718e930fb9a490970c002b15c5a3b500ed55
Atmanss <otmanssbariia@gmail.com>
2023-09-08 18:50:54 +0100

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+
+	char b;
+
+
+	for (b = 'z'; b <= 'a'; b++)
+	{
+		putchar(b);
+	}
+
+
+	putchar('\n');
+	return (0);
+
+
+}
