45bdda93e77c7d7cee274a37279f9ac9010997e3
Atmanss <otmanssbariia@gmail.com>
2023-09-08 18:04:00 +0100

    Alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,16 +3,16 @@
 /**
  * main - Prints the alphabetic
  *
- * Return: Always (Success)
+ * Return: 0  (Success)
  */
 int main(void)
 {
 	char c;
 
-	while (c <= 'z')
+	for (c = 'a'; c <= 'z'; c++)
+	{
 		putchar(c);
-	c++;
-
+	}
 	putchar('\n');
 	return (0);
 }
