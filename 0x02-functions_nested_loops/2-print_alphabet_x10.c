02bbc12f1856e1fe947223d482218036390f4f94
Atmanss <otmanssbariia@gmail.com>
2023-09-12 16:14:50 +0100

    Alphabet 10 lines

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,12 +8,11 @@ void print_alphabet_x10(void);
 {
 	int line. ch;
 
-	for (line = 0; line <=0; line++)
+	for (line = 0; line <= 0; line++)
 	{
 		for (ch = 'a'; ch <= 'z'; ch++)
 			_putchar(ch);
 		_putchar('\n');
-	
 	}
 
 }
