f7cc187b7ae6f3395790755e0ff4e79250ae39a1
Atmanss <otmanssbariia@gmail.com>
2023-09-12 16:09:43 +0100

    Alphabet 10 lines

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *print_alphabet - use _printchar function to print alphabet 10 times
+ *
+*/
+void print_alphabet_x10(void);
+{
+	int line. ch;
+
+	for (line = 0; line <=0; line++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+			_putchar(ch);
+		_putchar('\n');
+	
+	}
+
+}
