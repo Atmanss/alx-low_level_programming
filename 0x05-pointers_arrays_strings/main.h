a68a9c39b989363224bf965826a14f9168eb816b
Atmanss <otmanssbariia@gmail.com>
2023-09-18 11:41:33 +0100

    Pointers and Arrays

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -10,7 +10,6 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
-0x05-pointers_arrays_strings
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 
