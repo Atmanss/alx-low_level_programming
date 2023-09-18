682c644ab5e30523325dc12197998446ec880404
Atmanss <otmanssbariia@gmail.com>
2023-09-18 12:12:45 +0100

    Pointers and Arrays

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * void swap_int - swap the values between two variables
+ * swap_int - swap the values between two variables
  *
  * @a: Pointer to the integer to be swaped with b
  * @b: Pointer to the integer to be swaped with a
